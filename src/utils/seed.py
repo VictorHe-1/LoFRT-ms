c338a8ae93ff5c81ce88f076706c05f5e928d212
VictorHe <zhiyuhe@u.nus.edu>
2023-12-11 15:34:44 +0800

    Add logger and seed setter

---
+++ src/utils/seed.py
@@ -0,0 +1,21 @@
+"""random seed"""
+import random
+
+import numpy as np
+
+import mindspore as ms
+import mindspore.dataset as ds
+
+
+def set_seed(seed=0):
+    """
+    seed: seed int
+
+    Note: to ensure model init stability, rank_id is removed from seed.
+    """
+    # if rank is None:
+    #    rank = 0
+    random.seed(seed)
+    np.random.seed(seed)
+    ms.set_seed(seed)
+    ds.config.set_seed(seed)

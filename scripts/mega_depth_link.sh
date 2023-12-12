be2f9ef6ab4ac761f69c00d9bc4f858fa7a2c700
VictorHe <zhiyuhe@u.nus.edu>
2023-12-12 09:42:43 +0800

    Add mega depth link sh

---
+++ scripts/mega_depth_link.sh
@@ -0,0 +1,3 @@
+ln -sv /home/hezhiyu/datasets/loftr/phoenix /home/hezhiyu/loftr/data/megadepth/train
+ln -sv /home/hezhiyu/datasets/loftr/phoenix /home/hezhiyu/loftr/data/megadepth/test
+ln -s /home/hezhiyu/datasets/loftr/megadepth_indices/* /home/hezhiyu/loftr/data/megadepth/index

9bd16753a65b4e0327dbdfdf8769f1441809009e
VictorHe <zhiyuhe@u.nus.edu>
2023-12-12 10:03:43 +0800

    Add model builder

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,3 @@
 from .loftr import LoFTR
 from .utils.cvpr_ds_config import default_cfg
+from .builder import build_model
---
+++ src/models/builder.py
@@ -0,0 +1,20 @@
+import mindspore as ms
+from mindspore.amp import auto_mixed_precision
+
+from src.utils.misc import lower_config, flattenList
+from src.models import LoFTR
+from src.losses.loftr_loss import LoFTRLoss
+
+def build_model(config, pretrained_ckpt=None, **kwargs):
+    _config = lower_config(config)
+    model = LoFTR(config=_config['loftr'])
+    loss = LoFTRLoss(_config)
+
+    if pretrained_ckpt:
+        ms.load_checkpoint(pretrained_ckpt, model)
+        logger.info(f"Load \'{pretrained_ckpt}\' as pretrained checkpoint")
+
+    if 'amp_level' in kwargs:
+        auto_mixed_precision(model, amp_level=kwargs["amp_level"])
+        auto_mixed_precision(loss, amp_level=kwargs["amp_level"])
+    return model, loss

f4c6335f8edb3e319514a6109c433a52f166abd8
VictorHe <zhiyuhe@u.nus.edu>
2023-12-13 10:37:54 +0800

    Fix bug for model builder

--- src/models/builder.py
+++ src/models/builder.py
@@ -1,9 +1,13 @@
+import logging
 import mindspore as ms
 from mindspore.amp import auto_mixed_precision
 
 from src.utils.misc import lower_config, flattenList
 from src.models import LoFTR
 from src.losses.loftr_loss import LoFTRLoss
+
+_logger = logging.getLogger(__name__)
+
 
 def build_model(config, pretrained_ckpt=None, **kwargs):
     _config = lower_config(config)
@@ -12,7 +16,7 @@ def build_model(config, pretrained_ckpt=None, **kwargs):
 
     if pretrained_ckpt:
         ms.load_checkpoint(pretrained_ckpt, model)
-        logger.info(f"Load \'{pretrained_ckpt}\' as pretrained checkpoint")
+        _logger.info(f"Load \'{pretrained_ckpt}\' as pretrained checkpoint")
 
     if 'amp_level' in kwargs:
         auto_mixed_precision(model, amp_level=kwargs["amp_level"])

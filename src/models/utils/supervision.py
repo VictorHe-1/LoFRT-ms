263d1fab81fd412ba200a25703007f2805ce10be
VictorHe <zhiyuhe@u.nus.edu>
2023-12-19 19:53:56 +0800

    Add supervision (WIP)

--- src/models/utils/supervision.py
+++ src/models/utils/supervision.py
@@ -21,8 +21,8 @@ def repeat(tensor, pattern, c):
 def mask_pts_at_padded_regions(grid_pt, mask):
     """For megadepth dataset, zero-padding exists in images"""
     mask = repeat(mask, 'n h w -> n (h w) c', c=2)
-    grid_pt[~mask.bool()] = 0
-    return grid_pt
+    new_grid_pt = ops.where(mask.bool(), grid_pt, ops.zeros_like(grid_pt))
+    return new_grid_pt
 
 
 def create_meshgrid(
@@ -62,13 +62,13 @@ def create_meshgrid(
                  [[0., 1.],
                   [1., 1.]]]])
     """
-    xs: Tensor = ops.linspace(0, width - 1, width)
-    ys: Tensor = ops.linspace(0, height - 1, height)
+    xs = ops.linspace(0, width - 1, width)
+    ys = ops.linspace(0, height - 1, height)
     if normalized_coordinates:
         xs = (xs / (width - 1) - 0.5) * 2
         ys = (ys / (height - 1) - 0.5) * 2
     # generate grid by stacking coordinates
-    base_grid: Tensor = ops.stack(ops.meshgrid(xs, ys, indexing="ij"), axis=-1)  # WxHx2
+    base_grid = ops.stack(ops.meshgrid(xs, ys, indexing="ij"), axis=-1)  # WxHx2
     return base_grid.permute(1, 0, 2).unsqueeze(0)  # 1xHxWx2
 
 
@@ -89,13 +89,16 @@ def spvs_coarse(data, config, data_cols):
         - for megadepth dataset, there're 4 kinds of resolution {i, i_resize, c, f}
     """
     # 1. misc
-    data_cols.index("image0")
-    N, _, H0, W0 = data[data_cols.index("image0")].shape
-    _, _, H1, W1 = data[data_cols.index("image1")].shape
+    N, _, H0, W0 = data[0].shape  # data_cols.index("image0")
+    _, _, H1, W1 = data[2].shape  # data_cols.index("image1")
     scale = config['LOFTR']['RESOLUTION'][0]
-    scale0 = scale * data[data_cols.index("scale0")][:, None] if 'scale0' in data_cols else scale
-    scale1 = scale * data[data_cols.index("scale1")][:, None] if 'scale1' in data_cols else scale
-    h0, w0, h1, w1 = map(lambda x: x // scale, [H0, W0, H1, W1])
+    scale0 = scale * data[8][:, None]  # if 'scale0' in data_cols else scale  # 8
+    scale1 = scale * data[9][:, None]  # if 'scale1' in data_cols else scale  # 9
+
+    h0 = H0 // scale
+    w0 = W0 // scale
+    h1 = H1 // scale
+    w1 = W1 // scale
 
     # 2. warp grids
     # create kpts in meshgrid and resize them to image resolution
@@ -103,30 +106,28 @@ def spvs_coarse(data, config, data_cols):
     grid_pt0_i = scale0 * grid_pt0_c
     grid_pt1_c = ops.stop_gradient(create_meshgrid(h1, w1, False).reshape(1, h1 * w1, 2).tile((N, 1, 1)))
     grid_pt1_i = scale1 * grid_pt1_c
-
     # mask padded region to (0, 0), so no need to manually mask conf_matrix_gt
-    if 'mask0' in data:
-        grid_pt0_i = ops.stop_gradient(mask_pts_at_padded_regions(grid_pt0_i, data[data_cols.index("mask0")]))
-        grid_pt1_i = ops.stop_gradient(mask_pts_at_padded_regions(grid_pt1_i, data[data_cols.index("mask1")]))
-
+    grid_pt0_i = mask_pts_at_padded_regions(grid_pt0_i, data[15])  # 15
+    grid_pt1_i = mask_pts_at_padded_regions(grid_pt1_i, data[16])  # 16
+    grid_pt0_i = ops.stop_gradient(grid_pt0_i)
+    grid_pt1_i = ops.stop_gradient(grid_pt1_i)
     # warp kpts bi-directionally and resize them to coarse-level resolution
     # (no depth consistency check, since it leads to worse results experimentally)
     # (unhandled edge case: points with 0-depth will be warped to the left-up corner)
     _, w_pt0_i = ops.stop_gradient(warp_kpts(grid_pt0_i,
-                                             data[data_cols.index('depth0')],
-                                             data[data_cols.index('depth1')],
-                                             data[data_cols.index('T_0to1')],
-                                             data[data_cols.index('K0')],
-                                             data[data_cols.index('K1')]))
+                                             data[1],  # 1 data_cols.index('depth0')
+                                             data[3],  # 3 data_cols.index('depth1')
+                                             data[4],  # 4 data_cols.index('T_0to1')
+                                             data[6],  # 6 data_cols.index('K0')
+                                             data[7]))  # 7 data_cols.index('K1')
     _, w_pt1_i = ops.stop_gradient(warp_kpts(grid_pt1_i,
-                                             data[data_cols.index('depth1')],
-                                             data[data_cols.index('depth0')],
-                                             data[data_cols.index('T_1to0')],
-                                             data[data_cols.index('K1')],
-                                             data[data_cols.index('K0')]))
+                                             data[3],  # 3 data_cols.index('depth1')
+                                             data[1],  # 1 data_cols.index('depth0')
+                                             data[4],  # 4 data_cols.index('T_1to0')
+                                             data[7],  # 7 data_cols.index('K1')
+                                             data[6]))  # 6 data_cols.index('K0')
     w_pt0_c = w_pt0_i / scale1
     w_pt1_c = w_pt1_i / scale0
-
     # 3. check if mutual nearest neighbor
     w_pt0_c_round = w_pt0_c[:, :, :].round().long()
     nearest_index1 = w_pt0_c_round[..., 0] + w_pt0_c_round[..., 1] * w1
@@ -135,7 +136,11 @@ def spvs_coarse(data, config, data_cols):
 
     # corner case: out of boundary
     def out_bound_mask(pt, w, h):
-        return (pt[..., 0] < 0) + (pt[..., 0] >= w) + (pt[..., 1] < 0) + (pt[..., 1] >= h)
+        con1 = (pt[..., 0] < 0).astype(ms.int16)
+        con2 = (pt[..., 0] >= w).astype(ms.int16)
+        con3 = (pt[..., 1] < 0).astype(ms.int16)
+        con4 = (pt[..., 1] >= h).astype(ms.int16)
+        return (con1 | con2 | con3 | con4).bool()
 
     nearest_index1[out_bound_mask(w_pt0_c_round, w1, h1)] = 0
     nearest_index0[out_bound_mask(w_pt1_c_round, w0, h0)] = 0
@@ -146,11 +151,27 @@ def spvs_coarse(data, config, data_cols):
 
     # 4. construct a gt conf_matrix
     conf_matrix_gt = ops.zeros((N, h0 * w0, h1 * w1))
-    b_ids, i_ids = ops.where(correct_0to1 != 0)
-    j_ids = nearest_index1[b_ids, i_ids]
+    correct_0to1 = correct_0to1.astype(ms.int16)
+    # original codes:
+    # ids = ops.nonzero(correct_0to1 != 0).astype(ms.int32)
+    # b_ids, i_ids = ids[:, 0], ids[:, 1]
+    # j_ids = nearest_index1[b_ids, i_ids].astype(ms.int32)
+    # conf_matrix_gt[b_ids, i_ids, j_ids] = 1
 
-    conf_matrix_gt[b_ids, i_ids, j_ids] = 1
-    conf_matrix_gt = ops.stop_gradient(conf_matrix_gt)
+    # new codes:
+    mask = ops.zeros_like(conf_matrix_gt)
+    ids_mask = (correct_0to1 != 0)
+    # i_ids = ms.Tensor(i_ids, dtype=ms.int32)
+    # j_ids = ms.Tensor(j_ids, dtype=ms.int32)
+    # b_ids = ms.Tensor(b_ids, dtype=ms.int32)
+    new_conf_matrix_gt = ops.where(mask.bool(), ops.ones_like(conf_matrix_gt), conf_matrix_gt)
+
+    # mask_correct_0to1 = (correct_0to1 != 0).astype(ms.int16)  # (1, 6400)
+    # row_ids = mask_correct_0to1.unsqueeze(2).tile((1, 1, conf_matrix_gt.shape[1]))  # (1, 6400, 6400)
+    # nearest_index1: (1, 6400)
+    #col_ids = nearest_index1[b_ids, i_ids]
+    # conf_matrix_gt[b_ids, i_ids, j_ids] = 1
+    conf_matrix_gt = ops.stop_gradient(new_conf_matrix_gt)
     data.append(conf_matrix_gt)  # TODO: confirm data type
     data_cols.append("conf_matrix_gt")
     # data.update({'conf_matrix_gt': conf_matrix_gt})
@@ -170,44 +191,4 @@ def spvs_coarse(data, config, data_cols):
     return data, data_cols
 
 
-def compute_supervision_coarse(data, config, data_cols):
-    data_source = data[data_cols.index("dataset_name")]
-    if data_source.lower() in ['scannet', 'megadepth']:
-        spvs_coarse(data, config, data_cols)
-    else:
-        raise ValueError(f'Unknown data source: {data_source}')
-
-
-##############  ↓  Fine-Level supervision  ↓  ##############
-
-def spvs_fine(data, config, data_cols):
-    """
-    Update:
-        data (dict):{
-            "expec_f_gt": [M, 2]}
-    """
-    # 1. misc
-    # w_pt0_i, pt1_i = data.pop('spv_w_pt0_i'), data.pop('spv_pt1_i')
-    w_pt0_i, pt1_i = data[data_cols.index('spv_w_pt0_i')], data[data_cols.index('spv_pt1_i')]
-    scale = config['LOFTR']['RESOLUTION'][1]
-    radius = config['LOFTR']['FINE_WINDOW_SIZE'] // 2
-
-    # 2. get coarse prediction
-    b_ids, i_ids, j_ids = data[data_cols.index('b_ids')], data[data_cols.index('i_ids')], data[data_cols.index('j_ids')]
-
-    # 3. compute gt
-    scale = scale * data[data_cols.index('scale1')][b_ids] if 'scale0' in data_cols else scale
-    # `expec_f_gt` might exceed the window, i.e. abs(*) > 1, which would be filtered later
-    expec_f_gt = (w_pt0_i[b_ids, i_ids] - pt1_i[b_ids, j_ids]) / scale / radius  # [M, 2]
-    expec_f_gt = ops.stop_gradient(expec_f_gt)
-    data_cols.append("expec_f_gt")
-    data.append(expec_f_gt)
-    return data, data_cols
-
-
-def compute_supervision_fine(data, config, data_cols):
-    data_source = data[data_cols.index("dataset_name")]
-    if data_source.lower() in ['scannet', 'megadepth']:
-        return spvs_fine(data, config, data_cols)
-    else:
-        raise NotImplementedError
+##############   Fine-Level supervision is moved to the loftr model  ##############
